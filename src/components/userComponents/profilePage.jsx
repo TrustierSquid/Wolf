4890a51dc24c0261ae3620f7f37eb755e9897e66
Sam <enthunt.sh@gmail.com>
2024-08-14 10:35:28 -0400

    added new page for viewing profiles

---
+++ src/components/userComponents/profilePage.jsx
@@ -0,0 +1,10 @@
+
+
+
+export default function ProfilePage(){
+   return (
+      <>
+         <h1>Profile page</h1>
+      </>   
+   )
+}
---
+++ src/profile.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ProfilePage from '/src/components/userComponents/profilePage.jsx'
+import '/src/assets/index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <ProfilePage/>
+  </React.StrictMode>,
+)
